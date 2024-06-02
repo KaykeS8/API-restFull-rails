bcd7c400c1a74fd11085363f3bb04c7d16ba4a2f
kayke <kayke.simao@gmail.com>
2024-06-01 21:27:19 -0300

    ErrorSerializer adicionado para lidar com os erros de requisição sem Accept

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,10 @@
 class ApplicationController < ActionController::API
+  include ErrorSerializer
   before_action :ensure_json_request
 
   def ensure_json_request 
     unless request.headers["Accept"] =~ /vnd\.api\+json/
-      render :nothing => true, :status => 406
+      render json: ErrorSerializer.message_error("Ocorreu um erro"), :status => 406
     else
       unless request.get?
         return if request.headers["Content-Type"] =~ /vnd\.api\+json/
